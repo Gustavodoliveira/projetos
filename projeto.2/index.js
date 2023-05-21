7b091efc98c0e0041719ccf7741ba72807a18f5d
Gustavo <gustavod.oliveira@outlook.com>
2023-05-21 11:23:05 -0300

    site pronto

---
+++ projeto.2/index.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu')
+
+
+function ulmenu () {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+menu.addEventListener('click', ulmenu);
