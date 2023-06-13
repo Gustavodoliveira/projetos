e14eeef008843eaf0b731bce7921af31720413a7
Gustavo <gustavod.oliveira@outlook.com>
2023-06-13 16:25:31 -0300

    terminei as pag e começo do banco de dados

--- projeto.3/app.js
+++ projeto.3/app.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -11,9 +11,52 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+
+
 app.get("/", function(req,res){
-    res.render("home");
+    res.render("register");
 });
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save();
+    res.render("home")
+})
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+   if (User.findOne({email: username, password: password})) {
+    res.render("home")
+   } else {
+    console.log();
+   }
+})
 
 app.get("/about", function(req, res){
     res.render("about");
