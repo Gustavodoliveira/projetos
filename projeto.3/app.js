77f8e5eeb743700ea6424b76b75150da82be3720
Gustavo <gustavod.oliveira@outlook.com>
2023-06-15 17:06:47 -0300

    autentificaçao com google concluida

--- projeto.3/app.js
+++ projeto.3/app.js
@@ -8,7 +8,8 @@ const saltRounds = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -35,25 +36,55 @@ mongoose.set("useCreateIndex", true);
 */
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose, {usernameField: "email"});
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
 
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/home",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req,res){
     res.render("register");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"]})
+);
 
 app.get("/home", function(req, res){
     if (req.isAuthenticated()) {
@@ -62,6 +93,13 @@ app.get("/home", function(req, res){
         res.redirect("/login");
     }
 });
+
+app.get('/auth/google/home', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  })
 
 app.get("/register", function(req, res){
     res.render("register");
