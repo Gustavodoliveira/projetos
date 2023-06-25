62e802c23b2cc6cc259fd4978507399c90a20bfa
Gustavo <gustavod.oliveira@outlook.com>
2023-06-25 19:47:58 -0300

    nav-bar concluida

--- projeto.4/buy-and-sell/src/components/header/header.jsx
+++ projeto.4/buy-and-sell/src/components/header/header.jsx
@@ -1,24 +1,43 @@
 import "./header.css";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./logo.png"
+import {BiMenu} from "react-icons/bi";
+import {AiOutlineMenu} from "react-icons/ai"
 
 const logo = Logo;
+const menu = BiMenu
 
 
 
 
 function Header () {
+
+    const [className, setClassName] = useState("")
+
+
+    function menu () {
+        if ( className === 'active'){
+            return setClassName('')
+        } else {
+            return setClassName("active")
+        }
+            
+     
+    }
+
+
     return (
         <header>
-           <nav>
+           <nav className={className}>
            <img src= {logo} alt="" />
-            <ul>
+            <ul  >
                 <li><a href="#">Produtos</a></li>
                 <li><a href="#">Login</a></li>
                 <li><a href="#">Register</a></li>
                 <li><a href="#">Contatos</a></li>
                 <li><a href="#">Sobre</a></li>
             </ul>
+            <button  onClick= {menu}><AiOutlineMenu /></button>
            </nav>
         </header>
     )
