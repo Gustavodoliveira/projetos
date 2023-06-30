53fd70af0d4d42c60f816160b56d61b8821f8920
Gustavo <gustavod.oliveira@outlook.com>
2023-06-30 13:15:52 -0300

    Meet: section main concluida

--- projeto.4/buy-and-sell/src/pages/Home/Home.jsx
+++ projeto.4/buy-and-sell/src/pages/Home/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Header from '../../components/header/header'
 import Footer from '../../components/footter/footer'
 import "./home.css"
-//import service1 from "../../assets/service-1.jpg"
-//import service2 from "../../assets/service-2.png"
-//import service3 from "../../assets/service-3.jpg"
+import "../../assets/css/utils.css"
+//import service1 from "../../assets/service.svg"
+import service2 from "../../assets/service-2.png"
+import service3 from "../../assets/service-3.png"
 
 
 const homePage = () => {
@@ -16,26 +17,26 @@ const homePage = () => {
     text_btn= "COMEÇAR"
     />
     <main>
-      <section className="section-apresentation">
-             <h2>Crie seu negocio</h2>
-          <div className="section__container">
-                <div className="section__block-text">
-                    <div className="section__block-text-1">
-                        <h3>Te auxiliamos em seu negocio</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro magni quaerat fugit eaque doloribus facere corrupti cum tenetu</p>
-                    </div>
-                    <div className="section__block-text-1">
-                        <h3>Venda seu produto</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro magni quaerat fugit eaque doloribus facere corrupti cum tenetu mkdxbsvsoxio sxixnihbds knxsbx</p>
-                    </div>
-                 </div>
-                 <div className='section__photo'>
-                       <img className="section__photo-1" src="" alt="service-1" />
-                       <img className="section__photo-2" src="" alt="service-4" />
-                       <img className="section__photo-3" src="" alt="service-3" />
+      <section>
+             <h2 className='u-center_text'>Crie seu negocio</h2>
+                <div class="section__Content">
+                  <div className="section__block-text">
+                      <div className="section__block-text-1">
+                          <h3>Te auxiliamos em seu negocio</h3>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro magni quaerat fugit eaque doloribus facere corrupti cum tenetu</p>
+                      </div>
+                      <div className="section__block-text-1">
+                          <h3>Venda seu produto</h3>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro magni quaerat fugit eaque doloribus facere corrupti cum tenetu mkdxbsvsoxio sxixnihbds knxsbx</p>
+                      </div>
+                   </div>
+                   <div className="composition__container">
+                     <div className="composition">
+                           <img className="composition__photo compositon__photo--1" width="100%"src={service3} alt="service-4" />
+                           <img className="composition__photo compositon__photo--2" src={service2} alt="service-3" />
+                     </div>
+                   </div>
                 </div>
-            
-          </div>
       </section>
     </main>
     
